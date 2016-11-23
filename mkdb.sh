09eae4e7bd77585de93aa6e04e97cc50e5167024
avdemidov <avdemidov@users.noreply.github.com>
2016-11-24 01:39:42 +0300

    solution 2.3

--- mkdb.sh
+++ mkdb.sh
@@ -1,6 +1,6 @@
 mysql -uroot -p -e "
   CREATE DATABASE qa;
-  CREATE USER 'qa'@'localhos';
+  CREATE USER 'qa'@'localhost';
   GRANT ALL PRIVILEGES ON qa.* TO 'qa'@'localhost';
   FLUSH PRIVILEGES;
 "
